884a822f5b615159a9cf1845f1c3f81ff2d249e0
Shahed Ahmed <shahedahmedutsho@gmail.com>
2024-03-30 17:59:49 +0600

    Sign out add

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,25 +1,46 @@
-import { getAuth, signInWithPopup } from "firebase/auth";
+import { getAuth, signInWithPopup, signOut } from "firebase/auth";
 import app from "../../firebase/firebase.init";
 import { GoogleAuthProvider } from "firebase/auth";
+import { useState } from "react";
 
 
 const Login = () => {
+    const [user,setUser] =useState(null)
     const auth = getAuth(app)
-    console.log(app)
+    // console.log(app)
 const provider = new GoogleAuthProvider();
 const googleSignIn =()=>{
     signInWithPopup(auth,provider)
     .then(result => {
         const user =result.user
         console.log(user)
+        setUser(user)
     })
     .catch(error=>{
 console.log("error ---",error.message )
     })
 }
+const googleSignOut = () =>{
+    signOut(auth)
+    .then((res)=>{
+console.log("okey")
+console.log(res)
+setUser(null)
+    })
+    .catch(error =>{
+        console.log("error" , error.message)
+    })
+}
+
+
     return (
         <div>
         <button onClick={googleSignIn}>google</button>
+        <button onClick={googleSignOut}>sign out</button>
+        {user && <div>
+            <h3>User {user.displayName}</h3>
+            <h3>email : {user.email}</h3>
+        </div>}
         </div>
     );
 };
